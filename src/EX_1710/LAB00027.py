fa41b6104ecf8b0b4760318a92071ad564f65c50
Abhishek Upadhyay <abhishekkup9@gmail.com>
2026-02-12 14:28:10 +0530

    area of the circle

---
+++ src/EX_1710/LAB00027.py
@@ -0,0 +1,11 @@
+#Escape sequence
+print("Hello World")
+print("Hello\nWorld") #print world in a new line
+print("Hello\bWorld") # remove preveous letter (backspace)
+print("Hello\tWorld") # tab space
+
+#dir='c:\Abhishek\n.txt'
+#print(dir)
+
+dir=r"c:\Abhishek\n.txt"
+print(dir)
